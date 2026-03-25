a9bcd0fa520f2e9594f9c64212b7dfb8e1d16581
Xavier <xavier.cazalot@gmail.com>
2026-03-25 12:16:54 +0100

    Pydantic demo + PUT endpoint

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Coffee(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 
 @app.get("/")
@@ -11,3 +18,8 @@ def read_home():
 @app.get("/coffees/{coffee_id}")
 def read_coffee(coffee_id: int, q: str | None = None):
     return {"coffee_id": coffee_id, "q": q}
+
+
+@app.put("/coffees/{coffee_id}")
+def update_coffee(coffee_id: int, coffee: Coffee):
+    return {"coffee_name": coffee.name, "coffee_id": coffee_id}
