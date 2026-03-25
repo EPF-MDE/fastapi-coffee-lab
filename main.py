f1086e455577bd152dd555aebaafa1fa19358e0a
Xavier <xavier.cazalot@gmail.com>
2026-03-25 12:39:14 +0100

    Update operation connected to the in-memory DB

--- main.py
+++ main.py
@@ -35,4 +35,14 @@ def create_coffee(coffee_name: str, price: float, is_offer: bool | None):
 
 @app.put("/coffees/{coffee_id}")
 def update_coffee(coffee_id: int, coffee: Coffee):
-    return {"coffee_name": coffee.name, "coffee_id": coffee_id}
+    # print("coffees", coffees)
+
+    coffees[coffee_id].update(coffee)
+
+    # print("coffees[coffee_id]", coffees[coffee_id])
+
+    response = {"coffee_name": coffees[coffee_id]["name"], "coffee_id": coffee_id}
+
+    # print("response", response)
+
+    return response
